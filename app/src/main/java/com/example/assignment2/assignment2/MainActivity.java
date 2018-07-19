cc2b8a2a47140664625e9727f26189c117cbbccb
HamdaanAli <hamdan.ali749@gmail.com>
2018-07-19 08:37:36 +0530

    19/6/2018 time : 8:37

--- app/src/main/java/com/example/assignment2/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/assignment2/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.assignment2.assignment2;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -20,8 +24,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import au.com.bytecode.opencsv.CSVWriter;
 import io.reactivex.Observable;
@@ -34,51 +44,53 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     private Button loadContacts;
-    private CSVWriter writer=null;
+    private CSVWriter writer = null;
     private StringBuilder builder;
     private RelativeLayout relativeLayout;
     private Disposable disposable;
-    private static final String TAG =MainActivity.class.getSimpleName();
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_PICK_CONTACTS = 1;
+    private Uri uriContact;
+    private String contactID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        relativeLayout=(RelativeLayout) findViewById(R.id.relativelayout);
-        loadContacts=(Button) findViewById(R.id.LoadContacts);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout);
+        loadContacts = (Button) findViewById(R.id.LoadContacts);
         loadContacts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-                {
-                    if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED)
-                    {
-                        Toast.makeText(getApplicationContext(),"Permission Denied",Toast.LENGTH_LONG).show();
-                        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_CONTACTS},1);
-                    }else
-                    {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+                    } else {
 
 
-                     Observable<String> showContacts=getContact();
-                     Observer<String> observer=getContactsObserver();
-                     showContacts
-                             .observeOn(Schedulers.io())
-                             .subscribeOn(Schedulers.single())
-                             .subscribe(observer);
+                        Observable<String> showContacts = getContact();
+                        Observer<String> observer = getContactsObserver();
+                        showContacts
+                                .observeOn(Schedulers.io())
+                                .subscribeOn(Schedulers.newThread())
+                                .observeOn(Schedulers.computation())
+                                .subscribe(observer);
 
                     }
                 }
             }
         });
+
     }
 
+    @NonNull
     private String loadContacts() {
-        StringBuilder builder=new StringBuilder();
-        ContentResolver contentResolver=getContentResolver();
-        Cursor cursor= contentResolver.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
-        if(cursor.getCount()>0)
-        {
+        StringBuilder builder = new StringBuilder();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if (cursor.getCount() > 0) {
             while (cursor.moveToNext()) {
                 String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
                 String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
@@ -97,48 +109,41 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-       cursor.close();
+        cursor.close();
 
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-        {
-            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
-            {
-                Toast.makeText(getApplicationContext(),"Permission Denied",Toast.LENGTH_LONG).show();
-                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},2);
-            }else
-            {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+            } else {
 
 
-           createCSVFile(builder.toString());
+                createCSVFile(builder.toString());
 
             }
         }
-       return builder.toString();
+        return builder.toString();
     }
 
-    public void createCSVFile(String csv)
-    {
-        try
-        {
-            writer = new CSVWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/myfile.csv"), ',');
-
+    public void createCSVFile(String csv) {
+        try {
             String entries = csv;
+            writer = new CSVWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() + "/myfile.csv"), ',');
             writer.writeNext(new String[]{entries});
             writer.close();
             Snackbar snackbar = Snackbar
                     .make(relativeLayout, "File Saved into Internal Storage", Snackbar.LENGTH_LONG);
 
             snackbar.show();
-        }
-        catch (IOException e)
-        {
-            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
-            Log.d("ErrorInCSV",e.getStackTrace()+"");
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.d("ErrorInCSV", e.getStackTrace() + "");
 
         }
 
-   }
+    }
+
     private Observer<String> getContactsObserver() {
         return new Observer<String>() {
 
@@ -164,14 +169,36 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+
     private Observable<String> getContact() {
         return Observable.just(loadContacts());
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-      
+
         disposable.dispose();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static void zipFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            String zipFileName = file.getName().concat(".zip");
+
+            FileOutputStream fos = new FileOutputStream(zipFileName);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            zos.putNextEntry(new ZipEntry(file.getName()));
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            zos.write(bytes, 0, bytes.length);
+            zos.closeEntry();
+            zos.close();
+        } catch (FileNotFoundException ex) {
+            System.err.format("The file %s does not exist", filePath);
+        } catch (IOException ex) {
+            System.err.println("I/O error: " + ex);
+        }
     }
 }
